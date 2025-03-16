f2c76700b5ee12cbbffbc5eed24fb6906f944861
bizsam <34167131+bizsam@users.noreply.github.com>
2025-03-16 09:59:31 +0530

    Add files via upload

---
+++ pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"289d6b79f2956f20f833b7b2fe6c3e9b","url":"eslint.config.js"},{"revision":"cdce1c68f73ea8f29d11ec5862b9bd83","url":"index.html"},{"revision":"ef3ba2a81ba19f8ea5ddf625521e96f0","url":"postcss.config.js"},{"revision":"3061aee6b63300763d91be8ff5432a42","url":"src/App.css"},{"revision":"5a5bf3e0f55546b94b33fd5f60872ca9","url":"src/index.css"}]);
